49f1986f0c82b8800135efa9f6b81573aeb9f878
Minwoo <hemahero@naver.com>
2024-01-17 14:22:45 +0900

    init

--- utils/SDGclassfierBert/model.py
+++ utils/SDGclassfierBert/model.py
@@ -14,33 +14,5 @@ else:
     tokenizers = AutoTokenizer.from_pretrained(save_path)
     model = AutoModelForSequenceClassification.from_pretrained(save_path)
 
-# In[] print model_config state_dict
-def categorize_and_print_layers():
-    embedding_layers = []
-    attention_layers = []
-    layer_norm_layers = []
-    linear_layers = []
-    other_layers = []
-
-    for name, param in model.named_parameters():
-        if "embeddings" in name:
-            embedding_layers.append(name)
-        elif "attention" in name:
-            attention_layers.append(name)
-        elif "LayerNorm" in name:
-            layer_norm_layers.append(name)
-        elif "linear" in name or "dense" in name:
-            linear_layers.append(name)
-        else:
-            other_layers.append(name)
-
-    # Print each category
-    print("Embedding Layers:\n", embedding_layers, "\n")
-    print("Attention Layers:\n", attention_layers, "\n")
-    print("Layer Normalization Layers:\n", layer_norm_layers, "\n")
-    print("Linear (Dense) Layers:\n", linear_layers, "\n")
-    print("Other Layers:\n", other_layers, "\n")
-
-categorize_and_print_layers()
 # In[]
-print(model.summary())
+print(model)
