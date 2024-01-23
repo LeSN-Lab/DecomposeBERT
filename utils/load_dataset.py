0c1a9e216b5acabd718798bcbe0a340d06c7f556
hemaher0 <hemahero@naver.com>
2024-01-24 05:13:46 +0900

    ssd

--- utils/load_dataset.py
+++ utils/load_dataset.py
@@ -10,9 +10,13 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import nltk
+import multiprocessing
+from functools import partial
+
 # nltk.download('punkt')
 # nltk.download('stopwords')
 # nltk.download('wordnet')
+
 
 # In[]: Define Dataset class
 class SDGDataset(Dataset):
@@ -33,23 +37,21 @@ class SDGDataset(Dataset):
 
 
 # In[]: Preprocessing
-def prep_text(text):
+def prep_text(text, lemmatizer, stop_words):
     text = text.lower()
     text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
 
-    # Tokenization
-    #words = word_tokenize(text)
-    words = text
-    # Remove stopwords and digits
-    words = [word for word in words if word.isalpha() and word not in stopwords.words('english')]
-
-    # Lemmatization
-    lemmatizer = WordNetLemmatizer()
+    words = word_tokenize(text)
+    words = [word for word in words if word.isalpha() and word not in stop_words]
     words = [lemmatizer.lemmatize(word) for word in words]
 
-    clean_text = ' '.join(words)
-    return clean_text
+    return ' '.join(words)
 
+def parallel_prep_texts(texts, stop_words, n_jobs=4):
+    lemmatizer = WordNetLemmatizer()
+    with multiprocessing.Pool(n_jobs) as pool:
+        clean_texts = pool.map(partial(prep_text, lemmatizer=lemmatizer, stop_words=stop_words), texts)
+    return clean_texts
 
 def collate_fn(batch):
     # Separate source and target elements in the batch
@@ -70,17 +72,27 @@ def load_sdg(tokenizer, test_size=0.3, batch_size=128):
         os.mkdir('./data')
     if not os.path.isdir('./data/SDG'):
         os.mkdir('./data/SDG')
-    if not os.path.isfile('./data/SDG/testDataset.csv'):
+
+    if not os.path.isfile('./data/SDG/Dataset.csv'):
         print("Downloading SDG dataset...")
         df = pd.read_csv('https://zenodo.org/record/5550238/files/osdg-community-dataset-v21-09-30.csv?download=1', sep='\t')
-        df.to_csv('./data/SDG/testDataset.csv')
-    df = pd.read_csv('./data/SDG/testDataset.csv')
-    df['clean_text'] = df['text'].apply(lambda x: prep_text(x))
+        df.to_csv('./data/SDG/Dataset.csv')
+        print("Download has been completed")
+
+    if not os.path.isfile('./data/SDG/prepData.csv'):
+        df = pd.read_csv('./data/SDG/Dataset.csv')
+        stop_words = set(stopwords.words('english'))
+        print("Preprocessing texts...")
+        df['clean_text'] = parallel_prep_texts(df['text'].tolist(), stop_words, n_jobs=4)
+        df.to_csv('./data/SDG/prepData.csv', index=False)
+        print("Processing texts has been completed")
+
+    df = pd.read_csv('./data/SDG/prepData.csv')
 
     dataset = SDGDataset(df['clean_text'], df['sdg'], tokenizer)
     train_size = int(len(dataset) * (1 - test_size))
     test_size = len(dataset) - train_size
-
+    print(f"train size: {train_size} test size: {test_size}")
     train_dataset, test_dataset = train_test_split(dataset, test_size=test_size, random_state=2021)
 
     train_dataloader = DataLoader(
