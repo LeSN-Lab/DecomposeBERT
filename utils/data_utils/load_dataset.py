25f879c9404a71d15e40e4dba40e735a0d003444
Minwoo <hemahero@naver.com>
2024-01-26 16:47:28 +0900

    backups

--- utils/data_utils/load_dataset.py
+++ utils/data_utils/load_dataset.py
@@ -35,56 +35,35 @@ def one_hot_encode(labels, num_classes):
 
 
 # In[]: Define load datasets for pretrained
-def load_pretrained(df, text_column, label_column, tokenizer, batch_size, _one_hot_encode=True):
-    # split train dataset into train, validation and test sets
-    train_text, temp_text, train_labels, temp_labels = train_test_split(df[text_column], df[label_column],
-                                                                        random_state=2018,
-                                                                        test_size=0.3,
-                                                                        stratify=df[label_column])
-
-    valid_text, test_text, valid_labels, test_labels = train_test_split(temp_text, temp_labels,
-                                                                    random_state=2018,
-                                                                    test_size=0.5,
-                                                                    stratify=temp_labels)
+def load_dataloader(df, text_column, label_column, tokenizer, batch_size, _one_hot_encode=True, test=False):
+    df_x = df[text_column]
+    df_y = df[label_column]
 
     # get a number of classified labels
-    num_classes = len(np.unique(df[label_column]))
+    num_classes = len(np.unique(df_y))
 
     if _one_hot_encode:
-        train_labels = one_hot_encode(train_labels, num_classes)
-        valid_labels = one_hot_encode(valid_labels, num_classes)
-        test_labels = one_hot_encode(test_labels, num_classes)
+        df_y = one_hot_encode(df_y, num_classes)
 
     # tokenize and encode sequences in the training set
-    tokens_train = tokenizer.batch_encode_plus(
-        train_text.tolist(), padding=True, truncation=True, return_tensors='pt', max_length=512,
-    )
-    # tokenize and encode sequences in the validation set
-    tokens_valid = tokenizer.batch_encode_plus(
-        valid_text.tolist(), padding=True, truncation=True, return_tensors='pt', max_length=512,
-    )
-    # tokenize and encode sequences in the test set
-    tokens_test = tokenizer.batch_encode_plus(
-        test_text.tolist(), padding=True, truncation=True, return_tensors='pt', max_length=512,
+    tokens_df = tokenizer.batch_encode_plus(
+        df_x.tolist(), padding=True, truncation=True, return_tensors='pt', max_length=512,
     )
 
     # define datasets
-    train_data = TextDataset(tokens_train, train_labels)
-    valid_data = TextDataset(tokens_valid, valid_labels)
-    test_data = TextDataset(tokens_test, test_labels)
+    data = TextDataset(tokens_df, df_y)
 
     # define dataloaders for datasets
-    train_dataloader = DataLoader(
-        train_data, sampler=RandomSampler(train_data), batch_size=batch_size
-    )
-    valid_dataloader = DataLoader(
-        valid_data, sampler=SequentialSampler(valid_data), batch_size=batch_size
-    )
-    test_dataloader = DataLoader(
-        test_data, sampler=SequentialSampler(test_data), batch_size=batch_size
-    )
+    if test:
+        dataloader = DataLoader(
+            data, sampler=SequentialSampler(data), batch_size=batch_size
+        )
+    else:
+        dataloader = DataLoader(
+            data, sampler=RandomSampler(data), batch_size=batch_size
+        )
 
-    return train_dataloader, valid_dataloader, test_dataloader
+    return dataloader
 
 
 # In[]: Define sdg dataset loader
@@ -101,7 +80,20 @@ def load_sdg(tokenizer, batch_size=32):
         print("Download has been completed")
 
     df = pd.read_csv('./data/SDG/Dataset.csv')
-    return load_pretrained(df, 'text', 'sdg', tokenizer, batch_size)
+    train_df, temp_df = train_test_split(df,
+                                         random_state=2018,
+                                         test_size=0.3,
+                                         stratify=df["sdg"])
+
+    valid_df, test_df = train_test_split(temp_df,
+                                         random_state=2018,
+                                         test_size=0.5,
+                                         stratify=temp_df["sdg"])
+    train_dataloader = load_dataloader(df, 'text', 'sdg', tokenizer, batch_size)
+    valid_dataloader = load_dataloader(df, 'text', 'sdg', tokenizer, batch_size)
+    test_dataloader = load_dataloader(df, 'text', 'sdg', tokenizer, batch_size)
+
+    return train_dataloader, valid_dataloader, test_dataloader
 
 
 # In[]: Define math dataset loader
@@ -126,70 +118,35 @@ def load_math_dataset(hot_encode=True, inputTimestep=20):
     y_train = np.asarray(y_train)
     y_test = np.asarray(y_test)
 
-    if hot_encode:
-        y_train = to_categorical(y_train)
-        y_test = to_categorical(y_test)
-        num_tags = y_train.shape[1]
-    else:
-        num_tags = 6
+    train_df = pd.DataFrame({'Problem': x_train, 'Category': y_train})
+    test_df = pd.DataFrame({'Problem': x_test, 'Category': y_test})
+    return train_df, test_df
 
-    vocab_size = 5000
-    # inputTimestep = 20
-    oov_tok = "<OOV>"
+#
+#     if hot_encode:
+#         y_train = to_categorical(y_train)
+#         y_test = to_categorical(y_test)
+#         num_tags = y_train.shape[1]
+#     else:
+#         num_tags = 6
+#
+#     vocab_size = 5000
+#     # inputTimestep = 20
+#     oov_tok = "<OOV>"
+#
+#     tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+#     tokenizer.fit_on_texts(x_train)
+#     tokenizer.fit_on_texts(x_test)
+#
+#     x_train = tokenizer.texts_to_sequences(x_train)
+#     x_train = pad_sequences(x_train, maxlen=inputTimestep, padding="pre", truncating="post")
+#
+#     x_test = tokenizer.texts_to_sequences(x_test)
+#     x_test = pad_sequences(x_test, maxlen=inputTimestep, padding="pre", truncating="post")
+#
+#     return x_train, x_test, y_train, y_test, vocab_size, inputTimestep, num_tags
 
-    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
-    tokenizer.fit_on_texts(x_train)
-    tokenizer.fit_on_texts(x_test)
+df_t, df_T = load_math_dataset()
 
-    x_train = tokenizer.texts_to_sequences(x_train)
-    x_train = pad_sequences(x_train, maxlen=inputTimestep, padding="pre", truncating="post")
-
-    x_test = tokenizer.texts_to_sequences(x_test)
-    x_test = pad_sequences(x_test, maxlen=inputTimestep, padding="pre", truncating="post")
-
-    return x_train, x_test, y_train, y_test, vocab_size, inputTimestep, num_tags
-
-def load_math_dataset2(hot_encode=True, inputTimestep=20):
-    data = \
-        tfds.as_numpy(tfds.load('math_qa',
-                                batch_size=-1))
-    x_train = []
-    y_train = []
-    x_test = []
-    y_test = []
-
-    list_classes = {'gain': 0, 'general': 1, 'geometry': 2, 'other': 3,
-                    'physics': 4, 'probability': 5}
-    for i in range(len(data['test']['category'])):
-        y_test.append(list_classes[data['test']['category'][i].decode('utf-8')])
-        x_test.append(data['test']['Problem'][i].decode('utf-8'))
-    for i in range(len(data['train']['category'])):
-        y_train.append(list_classes[data['train']['category'][i].decode('utf-8')])
-        x_train.append(data['train']['Problem'][i].decode('utf-8'))
-
-    y_train = np.asarray(y_train)
-    y_test = np.asarray(y_test)
-
-    if hot_encode:
-        y_train = to_categorical(y_train)
-        y_test = to_categorical(y_test)
-        num_tags = y_train.shape[1]
-    else:
-        num_tags = 6
-
-    vocab_size = 5000
-    # inputTimestep = 20
-    oov_tok = "<OOV>"
-
-    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
-    tokenizer.fit_on_texts(x_train)
-    tokenizer.fit_on_texts(x_test)
-
-    x_train = tokenizer.texts_to_sequences(x_train)
-    x_train = pad_sequences(x_train, maxlen=inputTimestep, padding="pre", truncating="post")
-
-    x_test = tokenizer.texts_to_sequences(x_test)
-    x_test = pad_sequences(x_test, maxlen=inputTimestep, padding="pre", truncating="post")
-
-    return x_train, x_test, y_train, y_test, vocab_size, inputTimestep, num_tags
-
+print(len(df_t))
+print(len(df_T))
